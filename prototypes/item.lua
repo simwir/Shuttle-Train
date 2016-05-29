9eb7f223defed2d4fb64673ac037fc167af7ee71
ST-DDT <ST-DDT@gmx.de>
2016-05-29 16:58:41 +0200

    Add order attribute to items and technologies. Fixes #22

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -5,6 +5,7 @@ data:extend({
         icon = "__ShuttleTrain__/graphics/icon_shuttleTrain.png",
         flags = {"goes-to-quickbar"},
         subgroup = "transport",
+        order = "a[train-system]-f[shuttle-train]",
         place_result = "shuttleTrain",
         stack_size = 5,
     }
